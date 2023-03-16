0a75312a8499dc793865435e23eb36d3c2b5cafe
Leslie <leslie.rineau@etu.imt-lille-douai>
2023-03-16 02:53:43 +0100

    taquin focntionnel + retour

--- taquin_skel/lib/exo6b.dart
+++ taquin_skel/lib/exo6b.dart
@@ -1,80 +1,282 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-// ==============
-// Models
-// ==============
-
 math.Random random = new math.Random();
 
-class Tile {
-  Color color;
+int Xtouch = 2;
+int Ytouch = 2;
 
-  Tile(this.color);
-  Tile.randomColor() {
-    this.color = Color.fromARGB(
-        255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-  }
-}
+String textAppBar = 'Crop the lord';
 
-// ==============
-// Widgets
-// ==============
+Tile tile =
+    new Tile(imageURL: 'assets/images/pic.jpeg', alignment: Alignment(-1, -1));
 
 class DisplayImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Move it'),
+          title: Text(textAppBar),
           centerTitle: true,
         ),
         body: Center(
-          child: PositionedTiles(),
-        ));
+            child: Column(children: [
+          TaquinTransform(), //Ici
+          Container(
+              height: 200,
+              child:
+                  Image.network('assets/images/pic.jpeg', fit: BoxFit.cover)),
+        ])));
   }
 }
 
-class TileWidget extends StatelessWidget {
-  final Tile tile;
-
-  TileWidget(this.tile);
-
+class TaquinTransform extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return this.coloredBox();
-  }
-
-  Widget coloredBox() {
-    return Container(
-        color: tile.color,
-        child: Padding(
-          padding: EdgeInsets.all(70.0),
-        ));
-  }
+  _TaquinTransformState createState() => _TaquinTransformState();
 }
 
-class PositionedTiles extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => PositionedTilesState();
-}
+class _TaquinTransformState extends State<TaquinTransform> {
+  double _taille = 3.0;
+  List<Widget> _tiles = [];
+  List<int> listPosition = [];
+  int indexBlank = 2;
+  int lastChange = 0;
+  bool shuff = false;
 
-class PositionedTilesState extends State<PositionedTiles> {
-  List<Widget> tiles =
-      List<Widget>.generate(2, (index) => TileWidget(Tile.randomColor()));
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(children: tiles),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.sentiment_very_satisfied), onPressed: swapTiles),
+  void initState() {
+    indexBlank = random.nextInt((_taille * _taille).toInt());
+    _tiles = generateCroppedTileList(_taille.toInt(), Xtouch, Ytouch);
+    listPosition =
+        List.generate(_taille.toInt() * _taille.toInt(), (index) => index);
+    //_tiles.shuffle();
+    _tiles[indexBlank] =
+        Tile(alignment: Alignment(0, 0)).BlankTile(_taille.toInt());
+    shuffleTaquin();
+  }
+
+  _changeTaille() {
+    indexBlank = random.nextInt((_taille * _taille).toInt());
+    _tiles = generateCroppedTileList(_taille.toInt(), Xtouch, Ytouch);
+    listPosition =
+        List.generate(_taille.toInt() * _taille.toInt(), (index) => index);
+    //_tiles.shuffle();
+    _tiles[indexBlank] =
+        Tile(alignment: Alignment(0, 0)).BlankTile(_taille.toInt());
+    shuffleTaquin();
+  }
+
+  shuffleTaquin() {
+    shuff = true;
+    for (var k = 0; k < _taille * _taille; k++) {
+      List index = [
+        indexBlank - _taille,
+        indexBlank - 1,
+        indexBlank + 1,
+        indexBlank + _taille
+      ];
+      index.removeWhere(
+          (element) => element < 0 || element > _taille * _taille - 1);
+      _onTapTile(index[random.nextInt(index.length)]);
+    }
+
+    shuff = false;
+  }
+
+  bool possible(int x) {
+    return [
+      indexBlank - _taille,
+      indexBlank - 1,
+      indexBlank + 1,
+      indexBlank + _taille
+    ].contains(x);
+    //[0, 1, -1].contains((x % _taille) - (y % _taille));
+  }
+
+  _onTapTile(int index) {
+    //print("Blank :" + indexBlank.toString() + "/ Tap :" + index.toString());
+
+    if (possible(index)) {
+      _tiles.insert(indexBlank, _tiles.removeAt(index));
+      listPosition.insert(indexBlank, listPosition.removeAt(index));
+      indexBlank > index
+          ? _tiles.insert(index, _tiles.removeAt(indexBlank - 1))
+          : _tiles.insert(index, _tiles.removeAt(indexBlank + 1));
+      indexBlank > index
+          ? listPosition.insert(index, listPosition.removeAt(indexBlank - 1))
+          : listPosition.insert(index, listPosition.removeAt(indexBlank + 1));
+
+      if (!shuff) {
+        lastChange = indexBlank;
+      }
+      indexBlank = index;
+
+      // print(
+      //     "NewBlank :" + indexBlank.toString() + "/ Tap :" + index.toString());
+      // print("_______");
+
+      if (!shuff && win()) {
+        _showWinDialog(context);
+        shuffleTaquin();
+      }
+      setState(() {});
+    }
+  }
+
+  bool win() {
+    for (var k = 0; k < listPosition.length; k++) {
+      if (listPosition[k] != k) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  _undo() {
+    _onTapTile(lastChange);
+  }
+
+  _showWinDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Bravo!'),
+          content: Text('The lord has been fix!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Reset'),
+            ),
+          ],
+        );
+      },
     );
   }
 
-  swapTiles() {
-    setState(() {
-      tiles.insert(1, tiles.removeAt(0));
-    });
+  Widget build(BuildContext context) {
+    return Column(children: [
+      GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 1,
+        mainAxisSpacing: 1,
+        shrinkWrap: true,
+        crossAxisCount: _taille.toInt(),
+        children: _tiles
+            .map((tile) => Padding(
+                  padding: EdgeInsets.all(0.4),
+                  child: InkWell(
+                    child: tile,
+                    onTap: () => _onTapTile(_tiles.indexOf(tile)),
+                  ),
+                ))
+            .toList(),
+      ),
+      Slider(
+        value: _taille,
+        min: 2,
+        max: 12,
+        divisions: 10,
+        onChanged: (value) {
+          setState(() {
+            _taille = value;
+            _changeTaille();
+          });
+        },
+      ),
+      ElevatedButton(
+        onPressed: () {
+          _undo();
+        },
+        child: Text('Undo'),
+      ),
+    ]);
   }
+}
+
+class Tile {
+  String imageURL;
+  Alignment alignment;
+
+  Tile({this.imageURL = 'assets/images/pic.jpeg', this.alignment});
+
+  Widget croppedImageTile(int taille) {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 1 / taille,
+            heightFactor: 1 / taille,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget BlankTile(int taille) {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 1 / taille,
+            heightFactor: 1 / taille,
+            child: Container(
+                color: Color.fromARGB(255, random.nextInt(255),
+                    random.nextInt(255), random.nextInt(255)),
+                child: Padding(
+                  padding: EdgeInsets.all(70.0),
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+List<Widget> generateCroppedTileList(int taille, int xtouch, int ytouch) {
+  List<Widget> l = [];
+
+  for (var y = 1; y < taille + 1; y++) {
+    for (var x = 1; x < taille + 1; x++) {
+      double Align_x = (((x - 1) * (2)) / (taille - 1)) - 1;
+      double Align_y = (((y - 1) * (2)) / (taille - 1)) - 1;
+
+      // if (y != ytouch || x != xtouch) {
+      l.add(Padding(
+        padding: EdgeInsets.all(0.4),
+        child: InkWell(
+          child: Tile(alignment: Alignment(Align_x, Align_y))
+              .croppedImageTile(taille),
+        ),
+      ));
+      // } else {
+      //   l.add(Padding(
+      //     padding: EdgeInsets.all(0.4),
+      //     child: InkWell(
+      //       child:
+      //           Tile(alignment: Alignment(Align_x, Align_y)).BlankTile(taille),
+      //       // onTap: () {
+      //       //   if ([0, -1, 1].contains(xtouch - x) &&
+      //       //       [0, -1, 1].contains(ytouch - y)) {
+      //       //     setState(() {
+      //       //       Xtouch = x;
+      //       //       Ytouch = y;
+      //       //     });
+      //       //   }
+      //       // },
+      //     ),
+      //   ));
+      // }
+    }
+  }
+
+  return l;
 }
